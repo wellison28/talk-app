1ea884b9a75ae15e23bb53d16b62045210425591
Wellison Souza <wellison28@gmail.com>
2018-10-12 00:34:50 -0300

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import Routes from './Routes'
+import reducers from './reducers'
 
 export default props => (
-  <Routes />
+  <Provider store={ createStore(reducers) }>
+    <Routes />
+  </Provider>
 )
