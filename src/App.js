563fdf89bfda30dae6bcfee9876474a122e95aea
Wellison Souza <wellison28@gmail.com>
2018-10-11 22:50:31 -0300

    create extry point and first component FormLogin

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import FormLogin from './components/FormLogin';
+
+export default props => (
+  <FormLogin />
+)
---
+++ src/components/FormLogin.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+export default props => (
+  <View style={ styles.mainContainer }>
+    <View style={ styles.titleContainer }>
+      <Text style={ styles.title }>Whatsap Clone</Text>
+    </View>
+    <View style={ styles.inputContainer } >
+      <TextInput style={ styles.input } placeholder="E-mail" />
+      <TextInput style={ styles.input } placeholder="Senha" />
+      <Text style={ styles.messageSignup }>Ainda não tem cadastro? Cadastre-se</Text>
+    </View>
+    <View style={ styles.buttonContainer } >
+      <Button color="#115e54" title="Acessar" onPress={() => false } />
+    </View>        
+  </View>
+)
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 20
+  },
+  titleContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 25
+  },
+  messageSignup: {
+    fontSize: 18
+  },
+  inputContainer: {
+    flex: 2
+  },
+  input: {
+    fontSize: 20,
+    height: 45
+  },
+  buttonContainer: {
+    flex: 2
+  }
+})
