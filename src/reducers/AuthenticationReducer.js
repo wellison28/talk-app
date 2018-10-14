e13c8da9219e4a575a2a2c987502af90cb688e1f
Wellison Souza <wellison28@gmail.com>
2018-10-14 15:57:22 -0300

    refactor reducers file

--- src/reducers/AuthenticationReducer.js
+++ src/reducers/AuthenticationReducer.js
@@ -8,24 +8,21 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   console.log(action)
-  if(action.type === 'change_name'){
-    return { ...state, name: action.payload }
-  }
-  else if(action.type === 'change_email'){
-    return { ...state, email: action.payload }
-  }
-  else if(action.type === 'change_password'){
-    return { ...state, password: action.payload }
-  }
-  else if(action.type === 'registration_error'){
-    return { ...state, registrationError: action.payload }
-  }
-  else if(action.type === 'registration_success'){
-    return { ...state, password: '', name: '' }
-  }
-  else if(action.type === 'login_error'){
-    return { ...state, loginError: action.payload }
-  }
 
-  return state;
+  switch(action.type){
+    case 'change_name':
+      return { ...state, name: action.payload }
+    case 'change_email':
+      return { ...state, email: action.payload }
+    case 'change_password':
+      return { ...state, password: action.payload }
+    case 'registration_error':
+      return { ...state, registrationError: action.payload }
+    case 'registration_success';
+      return { ...state, password: '', name: '' }
+    case 'login_error':  
+      return { ...state, loginError: action.payload }
+    default:
+      return state
+  }
 }
