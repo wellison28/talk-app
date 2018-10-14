1d429c6059e1e79d1822da1b9f13881779f0da8f
Wellison Souza <wellison28@gmail.com>
2018-10-14 07:31:51 -0300

    setup middleware redux thunk

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
 
 import Routes from './Routes'
 import reducers from './reducers'
 
 class App extends Component {
   componentWillMount(){
-    
+
     firebase.initializeApp({
       apiKey: "AIzaSyBat5Qokq0-eLZyy2BTHRzIi6AsXvH7dc4",
       authDomain: "talk-app-af097.firebaseapp.com",
@@ -21,7 +22,7 @@ class App extends Component {
 
   render() {
     return (
-      <Provider store={ createStore(reducers) }>
+      <Provider store={ createStore(reducers, {}, applyMiddleware(ReduxThunk)) }>
         <Routes />
       </Provider>
     )    
