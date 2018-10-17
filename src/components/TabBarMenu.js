96fdf3edd80105dd581de5cf46b5f6b0d5a1d329
Wellison Souza <wellison28@gmail.com>
2018-10-16 23:36:49 -0300

    create component addContact and apply style

--- src/components/TabBarMenu.js
+++ src/components/TabBarMenu.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text , StatusBar, StyleSheet } from 'react-native';
+import { View, Text , StatusBar, StyleSheet, Image } from 'react-native';
 import { TabBar } from 'react-native-tab-view';
 
 export default props => (
   <View style={ styles.mainContainer }>
     <StatusBar backgroundColor="#114d44"/>
-    <View style={ styles.titleContainer }>
-      <Text style={ styles.title }>WhatsApp Clone</Text>
+    <View style={ styles.header }>
+      <View style={ styles.titleContainer }>
+        <Text style={ styles.title }>WhatsApp Clone</Text>
+      </View>
+      <View style={ styles.addContacContainer }>
+        <View style={ styles.addContactImg }>
+          <Image source={ require('../img/adicionar-contato.png') }/>
+        </View>
+        <View style={ styles.logoutContainer }>
+          <Text style={ styles.logoutText }>Sair</Text>
+        </View>
+      </View>
     </View>
     <TabBar { ...props } style={ styles.tabBar }/>
   </View>
@@ -18,14 +28,33 @@ const styles = StyleSheet.create({
     elevation: 4,
     marginBottom: 6
   },
+  header: { 
+    flexDirection: 'row', 
+    justifyContent: 'space-between' 
+  },
   titleContainer: {
     height: 50,
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   title: {
     color: '#fff',
     fontSize: 20,
-    marginLeft: 20
+    marginLeft: 20,
+  },
+  addContacContainer: { 
+    flexDirection: 'row', 
+    marginRight: 20 
+  },
+  addContactImg: { 
+    width: 50, 
+    justifyContent: 'center' 
+  },
+  logoutContainer: { 
+    justifyContent: 'center' 
+  },
+  logoutText: { 
+    fontSize: 20, 
+    color: '#fff' 
   },
   tabBar: {
     backgroundColor: '#115e54',
