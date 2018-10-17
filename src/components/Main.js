174435edabb0fe2eab2c6aed76de8fefb5f0ea76
Wellison Souza <wellison28@gmail.com>
2018-10-16 22:25:30 -0300

    install tab view and basic implementation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
 
-const Main = props => (
-  <View style={ styles.mainContainer }>
-    <Text>Pagina Principal</Text>
-  </View>
-)
+const Conversas = () => (
+  <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+);
 
-export default Main;
+const Contatos = () => (
+  <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+);
+
+export default class Main extends Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'first', title: 'Conversas' },
+      { key: 'second', title: 'Contatos' },
+    ],
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderTabBar = props => <TabBar {...props} style={styles.header} />;
+
+  _renderScene = SceneMap({
+    first: Conversas,
+    second: Contatos,
+  });
+
+  render() {
+    return (
+      <TabView
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderTabBar={this._renderTabBar}
+        onIndexChange={this._handleIndexChange}
+        initialLayout={{
+          width: Dimensions.get('window').width,
+        }}
+      />
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  mainContainer: {
-    marginTop: 100,
+  scene: {
+    flex: 1,
   }
-})
+});
